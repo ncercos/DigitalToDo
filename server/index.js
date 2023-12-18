d7a764274bf6e0f5c4f7b1a03077aadfc6c2cb6b
Nicholas <nicholas.cercos@gmail.com>
2023-12-17 23:32:03 -0500

    Server serves client files

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ const path = require('path')
 
 const app = express()
 const PORT = 3000
+const clientPath = path.resolve(__dirname, '../client/dist')
 
 app.use(express.json())
+app.use(express.static(clientPath))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
